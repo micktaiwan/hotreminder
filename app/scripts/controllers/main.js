ee0d24d19dd6782dbda6c9c5f8408dd2c373481c
Mickael Faivre-Macon <faivrem@gmail.com>
2013-10-31 19:30:15 +0100

    Affichage de données statiques

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,9 +2,9 @@
 
 angular.module('hotreminderApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
+    $scope.subjects = [
+      {subject: 'Script pour recharger la base'},
+      {subject: 'On ne passe plus de TU, la priorité c\'est les TI'},
+      {subject: 'On gèle les commits mardi soir'}
     ];
   });
