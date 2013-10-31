b03c47adc291e07bc1d4fe399e545db09235ff72
robinComa <robin.coma.public@gmail.com>
2013-10-31 23:37:47 +0100

    Add desktop notifications

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('hotreminderApp')
-  .controller('MainCtrl', function ($scope) {
+  .controller('MainCtrl', function ($scope, Notification) {
     $scope.subjects = [
       {subject: 'Script pour recharger la base', author: 'Franck L.', content:'Blabla\nJe teste\nComment je converti les retours à la ligne ?'},
       {subject: 'On ne passe plus de TU, la priorité c\'est les TI', author: 'Amandine V.', content: 'Super contenu non ?'},
@@ -10,4 +10,13 @@ angular.module('hotreminderApp')
     $scope.todo = [
       {subject: 'afficher les sujets par date mais aussi classés par jour (\'Today\', puis \'Yesterday\', puis \'Last week\')', author: 'Mickael'}
     ];
+
+    $scope.enableNotifications = function() {
+        Notification.enableNotifications();
+    };
+
+    $scope.addNotification = function(){
+        Notification.addNotifications(Notification.types.WARNING, this.notification);
+    };
+
   });
