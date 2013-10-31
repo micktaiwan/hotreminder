68fbb6de3a5502a241e928a831203e2036f0c3d0
Mickael Faivre-Macon <faivrem@gmail.com>
2013-10-31 19:50:37 +0100

    better subjects styles

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -3,8 +3,8 @@
 angular.module('hotreminderApp')
   .controller('MainCtrl', function ($scope) {
     $scope.subjects = [
-      {subject: 'Script pour recharger la base'},
-      {subject: 'On ne passe plus de TU, la priorité c\'est les TI'},
-      {subject: 'On gèle les commits mardi soir'}
+      {subject: 'Script pour recharger la base', author: 'Franck L.'},
+      {subject: 'On ne passe plus de TU, la priorité c\'est les TI', author: 'Amandine V.'},
+      {subject: 'On gèle les commits mardi soir', author: 'Mickael F.'}
     ];
   });
