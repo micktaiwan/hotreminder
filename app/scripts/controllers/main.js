2ae2be6944d5c029b19b901de0dccef7ffeed914
robinComa <robin.coma.public@gmail.com>
2013-11-01 00:09:05 +0100

    Conflits main.hmlt resolved

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,22 +1,23 @@
 'use strict';
 
 angular.module('hotreminderApp')
-  .controller('MainCtrl', function ($scope, Notification) {
-    $scope.subjects = [
-      {subject: 'Script pour recharger la base', author: 'Franck L.', content:'Blabla\nJe teste\nComment je converti les retours à la ligne ?'},
-      {subject: 'On ne passe plus de TU, la priorité c\'est les TI', author: 'Amandine V.', content: 'Super contenu non ?'},
-      {subject: 'On gèle les commits mardi soir', author: 'Mickael F.', content: 'Bla bla', show: true}
-    ];
-    $scope.todo = [
-      {subject: 'afficher les sujets par date mais aussi classés par jour (\'Today\', puis \'Yesterday\', puis \'Last week\')', author: 'Mickael'}
-    ];
+    .controller('MainCtrl', function ($scope, Notification) {
+        $scope.subjects = [
+            {show:false, subject: 'Script pour recharger la base', author: 'Franck L.', content:'Blabla\nJe teste\nComment je converti les retours à la ligne ?', states: [{user_id_1: {state:'new'}}]},
+            {show:false, subject: 'On ne passe plus de TU, la priorité c\'est les TI', author: 'Amandine V.', content: 'Super contenu non ?', states: [{user_id_1: {state:'new'}}]},
+            {show:false, subject: 'On gèle les commits mardi soir', author: 'Mickael F.', content: 'Bla bla', states: [{user_id_1: {state:'new'}}]},
+            {show:false, subject: 'Avant de committer relisez la page du wiki qui défini la notion de \'fini\'', author: 'Mickael F.', content: 'Ouais parce que c\'est important...', states: [{user_id_1: {state:'new'}}]}
+        ];
+        $scope.todo = [
+            {subject: 'Firebase :)', author: 'Mickael'}
+        ];
 
-    $scope.enableNotifications = function() {
-        Notification.enableNotifications();
-    };
+        $scope.enableNotifications = function() {
+            Notification.enableNotifications();
+        };
 
-    $scope.addNotification = function(){
-        Notification.addNotifications(Notification.types.WARNING, this.notification);
-    };
+        $scope.addNotification = function(){
+            Notification.addNotifications(Notification.types.WARNING, this.notification);
+        };
 
-  });
+    });
